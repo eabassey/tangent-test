ba198d2cb327817e1956e1aba4d32b9002114f69
Enoch Abassey <eabassey@yahoo.com>
2018-04-25 23:43:26 +0200

    adding app.routing to lazy load main module

---
+++ src/app/app.routing.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full'
+  },
+  {
+    path: 'main',
+    loadChildren: 'app/main/main.module#MainModule'
+  }
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
