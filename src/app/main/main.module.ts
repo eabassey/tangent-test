57114fcdaf284f0c681b66e00619a5792f17ff3e
Enoch Abassey <eabassey@yahoo.com>
2018-04-25 23:34:52 +0200

    adding main component ts, scss and html files

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainRoutingModule } from './main.routing';
 
+import { MainComponent } from './main.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [MainComponent],
   imports: [CommonModule, MainRoutingModule]
 })
 export class MainModule {}
