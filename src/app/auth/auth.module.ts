15db4d7213ef77e9ae81117b9136f2ffe6b9e3b0
Enoch Abassey <eabassey@yahoo.com>
2018-04-26 00:00:25 +0200

    adding routing module for AuthModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { AuthRoutingModule } from './auth.routing';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule]
+  imports: [CommonModule, AuthRoutingModule]
 })
 export class AuthModule {}
