a5bbc10a14824d9572a675b8bfb18e7767dc4254
Enoch Abassey <eabassey@yahoo.com>
2018-04-26 13:38:48 +0200

    generated user-menu component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { UserMenuComponent } from './components/user-menu/user-menu.component';
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent, UserMenuComponent],
   imports: [CommonModule],
   exports: [HeaderComponent, FooterComponent, SidebarComponent]
 })
