6c75bcf2cc66b5b6b5aea93b39b4175b2ac0ba79
Enoch Abassey <eabassey@yahoo.com>
2018-04-26 15:33:37 +0200

    removing comments and changing view encapsulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { AppSettings } from './app-settings.model';
+import { Router } from '@angular/router';
+import { AppSettingsService } from './app-settings.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class AppComponent {}
+export class AppComponent {
+  public settings: AppSettings;
+  constructor(
+    public appSettingsService: AppSettingsService,
+    private router: Router
+  ) {
+    this.settings = this.appSettingsService.settings;
+  }
+}
