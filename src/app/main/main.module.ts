0c33902ab9df362731f8f1b55a16a71e1f23acf8
Enoch Abassey <eabassey@yahoo.com>
2018-04-26 12:45:56 +0200

    header layout

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,9 +5,16 @@ import { MainRoutingModule } from './main.routing';
 import { MainComponent } from './main.component';
 import { DashboardModule } from './dashboard/dashboard.module';
 import { EmployeesModule } from './employees/employees.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [CommonModule, MainRoutingModule, DashboardModule, EmployeesModule]
+  imports: [
+    CommonModule,
+    MainRoutingModule,
+    SharedModule,
+    DashboardModule,
+    EmployeesModule
+  ]
 })
 export class MainModule {}
