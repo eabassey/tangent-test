16807f742595ca6bc5beb9da5156df743bee8f4b
Enoch Abassey <eabassey@yahoo.com>
2018-04-26 23:16:47 +0200

    placed searchpipe outside folder

--- src/app/shared/pipes/pipes.module.ts
+++ src/app/shared/pipes/pipes.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { EmployeesSearchPipe } from './employees-search/employees-search.pipe';
+import { EmployeesSearchPipe } from './employees-search.pipe';
 
 @NgModule({
   imports: [CommonModule],
