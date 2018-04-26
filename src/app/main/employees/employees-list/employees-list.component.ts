652a7cdcce021da8cc8d85444021285d33d36ce8
Enoch Abassey <eabassey@yahoo.com>
2018-04-26 22:38:16 +0200

    laying out employee list view

--- src/app/main/employees/employees-list/employees-list.component.ts
+++ src/app/main/employees/employees-list/employees-list.component.ts
@@ -7,10 +7,41 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None
 })
 export class EmployeesListComponent implements OnInit {
+  public employees: any[];
+  public searchText: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.employees = this.getEmployees();
   }
 
+  getEmployees() {
+    return [
+      {
+        first_name: 'Kim',
+        last_name: 'Jonyung',
+        is_active: true,
+        position: {
+          name: 'Software Tester',
+          level: 'Senior'
+        },
+        email: 'kim@gmail.com',
+        gender: 'M',
+        race: 'B'
+      },
+      {
+        first_name: 'Donald',
+        last_name: 'Trump',
+        is_active: true,
+        position: {
+          name: 'Software Architect',
+          level: 'Senior'
+        },
+        email: 'donald@gmail.com',
+        gender: 'M',
+        race: 'W'
+      }
+    ];
+  }
 }
--- src/app/main/employees/employees.module.ts
+++ src/app/main/employees/employees.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { EmployeesComponent } from './employees.component';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
 
 @NgModule({
   declarations: [EmployeesComponent, EmployeesListComponent],
-  imports: [CommonModule, RouterModule]
+  imports: [CommonModule, RouterModule, FormsModule]
 })
 export class EmployeesModule {}
