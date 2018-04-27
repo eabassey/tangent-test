5f67cba3ff851821b7d1b5bf358b91639b8ebc1e
Enoch Abassey <eabassey@yahoo.com>
2018-04-27 11:33:11 +0200

    generated top-panel component in dashboard module

--- src/app/main/dashboard/dashboard.module.ts
+++ src/app/main/dashboard/dashboard.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { TopPanelComponent } from './top-panel/top-panel.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
+  declarations: [DashboardComponent, TopPanelComponent],
   imports: [CommonModule, NgxChartsModule, PerfectScrollbarModule]
 })
 export class DashboardModule {}
---
+++ src/app/main/dashboard/top-panel/top-panel.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-top-panel',
+  templateUrl: './top-panel.component.html',
+  styleUrls: ['./top-panel.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class TopPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
