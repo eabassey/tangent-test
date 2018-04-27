a2d74b95279a8fb7c15803638065f2e559e9181a
Enoch Abassey <eabassey@yahoo.com>
2018-04-27 14:17:07 +0200

    generated bottom-panel component in dashboard module

---
+++ src/app/main/dashboard/bottom-panel/bottom-panel.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-bottom-panel',
+  templateUrl: './bottom-panel.component.html',
+  styleUrls: ['./bottom-panel.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class BottomPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/main/dashboard/dashboard.module.ts
+++ src/app/main/dashboard/dashboard.module.ts
@@ -4,9 +4,10 @@ import { DashboardComponent } from './dashboard.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 import { TopPanelComponent } from './top-panel/top-panel.component';
+import { BottomPanelComponent } from './bottom-panel/bottom-panel.component';
 
 @NgModule({
-  declarations: [DashboardComponent, TopPanelComponent],
+  declarations: [DashboardComponent, TopPanelComponent, BottomPanelComponent],
   imports: [CommonModule, NgxChartsModule, PerfectScrollbarModule]
 })
 export class DashboardModule {}
