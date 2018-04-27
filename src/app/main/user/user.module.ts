6abb455784ca9458a664a0b4a359be9eb0884455
Enoch Abassey <eabassey@yahoo.com>
2018-04-27 16:51:15 +0200

    generated user-profile component

--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { RouterModule } from '@angular/router';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 @NgModule({
-  declarations: [UserComponent],
+  declarations: [UserComponent, UserProfileComponent],
   imports: [CommonModule, RouterModule]
 })
 export class UserModule {}
