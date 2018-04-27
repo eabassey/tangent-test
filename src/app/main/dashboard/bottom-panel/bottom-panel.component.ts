f9090a433a5b132936f0b1a89d9b6f52b4fae44b
Enoch Abassey <eabassey@yahoo.com>
2018-04-27 16:37:34 +0200

    laying out bottom-panel component with demo data

--- src/app/main/dashboard/bottom-panel/bottom-panel.component.ts
+++ src/app/main/dashboard/bottom-panel/bottom-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-bottom-panel',
@@ -6,11 +6,30 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./bottom-panel.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class BottomPanelComponent implements OnInit {
+export class BottomPanelComponent implements OnInit, OnDestroy {
+  public colorScheme = {
+    domain: ['#FFFFFF']
+  };
+  autoScale = true;
+  animations = true;
+  gradient = true;
+  view = [700, 300];
 
-  constructor() { }
+  gender = [{ name: 'Male', value: 12 }, { name: 'Female', value: 21 }];
+  status = [{ name: 'Active', value: 31 }, { name: 'Inactive', value: 11 }];
+  race = [
+    { name: 'Black', value: 31 },
+    { name: 'White', value: 11 },
+    { name: 'Indian', value: 21 }
+  ];
+  level = [{ name: 'Senior', value: 31 }, { name: 'Junior', value: 21 }];
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  public onSelect(event) {
+    console.log(event);
   }
 
+  ngOnDestroy() {}
 }
