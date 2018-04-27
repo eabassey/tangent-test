8e1c765c8801c6f82052e122f1bfd1e7b6a38e03
Enoch Abassey <eabassey@yahoo.com>
2018-04-27 14:16:15 +0200

    laying out top-panel with number charts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app.routing';
 
@@ -8,7 +10,12 @@ import { AppSettingsService } from './app-settings.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ErrorsModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ErrorsModule
+  ],
   providers: [AppSettingsService],
   bootstrap: [AppComponent]
 })
--- src/app/main/dashboard/top-panel/top-panel.component.ts
+++ src/app/main/dashboard/top-panel/top-panel.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, DoCheck, ViewEncapsulation } from '@angular/core';
+import { AppSettings } from '../../../app-settings.model';
+import { AppSettingsService } from '../../../app-settings.service';
 
 @Component({
   selector: 'app-top-panel',
@@ -6,11 +8,30 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   styleUrls: ['./top-panel.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class TopPanelComponent implements OnInit {
+export class TopPanelComponent {
+  public allEmployees = [{ name: 'All Employees', value: 21 }];
+  public allEmployeesBgColor = { domain: ['#F47B00'] };
 
-  constructor() { }
+  public staffEmployees = [{ name: 'Staff Employees', value: 789 }];
+  public staffEmployeesBgColor = { domain: ['#2F3E9E'] };
 
-  ngOnInit() {
+  public birthDaysThisMonth = [{ name: 'Birthdays This Month', value: 7 }];
+  public birthDaysThisMonthBgColor = { domain: ['#606060'] };
+
+  public infoLabelFormat(c): string {
+    switch (c.data.name) {
+      case 'All Employees':
+        return `<i class="fa fa-users mr-2"></i>${c.label}`;
+      case 'Staff Employees':
+        return `<i class="fa fa-user mr-2"></i>${c.label}`;
+      case 'Birthdays This Month':
+        return `<i class="fa fa-birthday-cake mr-2"></i>${c.label}`;
+      default:
+        return c.label;
+    }
   }
 
+  public onSelect(event) {
+    console.log(event);
+  }
 }
