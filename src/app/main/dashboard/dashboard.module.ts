e757294aac4e4e5937a51c6f2790808cff6ac6af
Enoch Abassey <eabassey@yahoo.com>
2018-04-27 11:19:09 +0200

    installing ngxcharts package

--- src/app/main/dashboard/dashboard.module.ts
+++ src/app/main/dashboard/dashboard.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
-
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule]
+  imports: [CommonModule, NgxChartsModule, PerfectScrollbarModule]
 })
 export class DashboardModule {}
