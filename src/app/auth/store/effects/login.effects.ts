6a3b04efa657347c88d867a62ec2cbb1bc918fcc
Enoch Abassey <eabassey@yahoo.com>
2018-04-30 18:05:17 +0200

    using ng4-spinner for login effect

--- src/app/auth/store/effects/login.effects.ts
+++ src/app/auth/store/effects/login.effects.ts
@@ -6,22 +6,28 @@ import { switchMap, map, catchError, tap } from 'rxjs/operators';
 import { of } from 'rxjs/observable/of';
 import { AuthService } from '../../services/auth.service';
 import { Router } from '@angular/router';
+import { Ng4LoadingSpinnerService } from 'ng4-loading-spinner';
 
 @Injectable()
 export class LoginEffects {
   constructor(
     private actions: Actions,
     private authService: AuthService,
-    private router: Router
+    private router: Router,
+    private spinner: Ng4LoadingSpinnerService
   ) {}
 
   @Effect()
   login$ = this.actions.ofType(fromActions.LOGIN).pipe(
     switchMap((action: fromActions.Login) => {
+      this.spinner.show();
       const { username, password } = action.payload;
       return this.authService.login({ username, password });
     }),
-    map(token => new fromActions.UseTokenToAccess(token))
+    map(token => {
+      this.spinner.hide();
+      return new fromActions.UseTokenToAccess(token);
+    })
   );
 
   @Effect()
@@ -29,10 +35,14 @@ export class LoginEffects {
     .ofType(fromActions.USE_TOKEN_TO_ACCCESS)
     .pipe(
       switchMap((action: fromActions.UseTokenToAccess) => {
+        this.spinner.show();
         const token = action.payload;
         return this.authService.getLoggedInUserInfo(token);
       }),
-      tap(() => this.router.navigate(['/main'])),
+      tap(() => {
+        this.spinner.hide();
+        this.router.navigate(['/main']);
+      }),
       map(userInfo => new fromActions.LoginSuccess(userInfo)),
       catchError(error => of(new fromActions.LoginFail(error)))
     );
